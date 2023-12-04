86aafc340b72e911347e8bc1e14e7a5d43527be8
pocketlinux32 <pocketlinux32@gmail.com>
2023-12-04 02:07:24 -0600

    typo

--- pl-srv.c
+++ pl-srv.c
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]){
 
 	if(argc > 1){
 		if(strcmp("help", argv[1]) == 0){
-			puts("PortaLinux Service Supervisor v0.04");
+			puts("PortaLinux Service Supervisor v0.05");
 			puts("(c) 2023 pocketlinux32, Under MPL 2.0\n");
 			printf("Usage: %s {options} [value]\n\n", argv[0]);
 			puts("Starts and supervises a service. All service units are stored in /etc/pl-srv");
